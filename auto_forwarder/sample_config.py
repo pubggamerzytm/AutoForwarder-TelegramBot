7203bfa721fe53134c53389bc7d0e0f926d52793
pubggamerzytm <70851685+pubggamerzytm@users.noreply.github.com>
2020-12-26 21:19:55 +0530

    Update sample_config.py

--- auto_forwarder/sample_config.py
+++ auto_forwarder/sample_config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "1452142207:AAEdOgi8v4FDD2PpToeYplCUjcnSIZrNDTU"  # API key obtained from BotFather
+    OWNER_ID = "1119811251"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297 ]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
+    FROM_CHATS = [-442278762 ]  # List of chat id's to forward messages from
+    TO_CHATS = [-442764193]  # List of chat id's to forward messages to
 
     # FOR WEBHOOKS
     WEBHOOK = False
